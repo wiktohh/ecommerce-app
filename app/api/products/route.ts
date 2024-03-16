bcb77fc80d19d5e99b4157da82b12a2696fcf82a
wiktohh <wiktor.rzeznicki01@gmail.com>
2024-03-16 15:19:49 +0100

    feat: add pagination

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,10 +1,19 @@
 import prisma from "@/app/lib/prisma";
-import { NextResponse } from "next/server";
+import { NextApiRequest } from "next";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(req: NextRequest) {
   try {
-    const allProducts = await prisma.product.findMany();
-    return NextResponse.json(allProducts);
+    const page = req.nextUrl.searchParams.get("page") || 1;
+    const limit = req.nextUrl.searchParams.get("limit") || 10;
+
+    const productsLength = await prisma.product.count();
+
+    const popularProducts = await prisma.product.findMany({
+      take: Number(limit) || 10,
+      skip: (Number(page) - 1) * Number(limit) || 0,
+    });
+    return NextResponse.json({ popularProducts, productsLength });
   } catch (e) {
     return new NextResponse((e as Error).message, { status: 500 });
   }
--- app/products/components/ProductsList/ProductsList.tsx
+++ app/products/components/ProductsList/ProductsList.tsx
@@ -2,23 +2,43 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Product from "./Product";
-import { collectGenerateParams } from "next/dist/build/utils";
 import LoadingSpinner from "@/app/components/LoadingSpinner";
+import Button from "@/app/components/Button";
 
 const ProductsList = () => {
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalProducts, setTotalProducts] = useState(0);
+  const [itemsPerPage] = useState(10);
+
+  const totalPages = Math.ceil(totalProducts / itemsPerPage);
 
   useEffect(() => {
-    setIsLoading(true);
     const fetchProducts = async () => {
-      const response = await axios.get("/api/products");
+      setIsLoading(true);
+      const response = await axios.get(
+        `/api/products?page=${currentPage}&limit=${itemsPerPage}`
+      );
       const data = await response.data;
-      setProducts(data);
+      setProducts(data.popularProducts);
+      setTotalProducts(data.productsLength);
       setIsLoading(false);
     };
     fetchProducts();
-  }, []);
+  }, [currentPage]);
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
 
   return (
     <div className="w-5/6">
@@ -29,11 +49,27 @@ const ProductsList = () => {
             <LoadingSpinner />
           </div>
         ) : (
-          <div className="flex justify-center flex-wrap gap-16">
-            {products.map((product, idx) => (
-              <Product key={idx} product={product} />
-            ))}
-          </div>
+          <>
+            <div className="flex justify-center flex-wrap gap-16">
+              {products.map((product, idx) => (
+                <Product key={idx} product={product} />
+              ))}
+            </div>
+            <div className="flex justify-center items-center mt-4">
+              <Button onClick={handlePrevPage} disabled={currentPage === 1}>
+                Poprzednia
+              </Button>
+              <span className="mx-4">
+                {currentPage} z {totalPages}
+              </span>
+              <Button
+                onClick={handleNextPage}
+                disabled={currentPage === totalPages}
+              >
+                Następna
+              </Button>
+            </div>
+          </>
         )}
       </div>
     </div>
