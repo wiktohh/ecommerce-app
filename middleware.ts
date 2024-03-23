aa7724e91666d75916b5f7ea380b4b7673cd7ae6
wiktohh <wiktor.rzeznicki01@gmail.com>
2024-03-23 21:19:13 +0100

    feat: add middleware

---
+++ middleware.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest) {
+  const token = req.cookies.get("next-auth.session-token")?.value;
+  const nextPathname = req.nextUrl.pathname;
+
+  if (!token && nextPathname.startsWith("/account")) {
+    return NextResponse.redirect(`${process.env.HOST_NAME}/auth`);
+  }
+}
+
+export const config = {
+  matcher: ["/account"],
+};
