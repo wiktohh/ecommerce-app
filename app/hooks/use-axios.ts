94cbb70b184443cdd8ec87614f3d4628fd801b6d
wiktohh <wiktor.rzeznicki01@gmail.com>
2024-03-27 11:59:56 +0100

    refactor: change url in use-axios

--- app/hooks/use-axios.ts
+++ app/hooks/use-axios.ts
@@ -9,7 +9,7 @@ const getToken = () => {
 
 export const useAxios = () => {
   const instance = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: process.env.HOST_NAME as string,
   });
 
   instance.interceptors.request.use(
