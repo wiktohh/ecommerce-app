03e5b3d387268524dd9a00562407af148c1757c0
wiktohh <wiktor.rzeznicki01@gmail.com>
2024-03-28 09:52:17 +0100

    feat: add docker

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,10 +1,5 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../generated/client";
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+const prisma = new PrismaClient();
 
-const client = globalThis.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
-
-export default client;
+export default prisma;
