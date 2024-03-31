a5a8484457862065d3f4ba53108df8a5973bbf4d
wiktohh <wiktor.rzeznicki01@gmail.com>
2024-03-31 11:34:48 +0200

    refactor: add redirect to home page when cancel page doesnt have session_id

--- app/cancel/page.tsx
+++ app/cancel/page.tsx
@@ -1,6 +1,18 @@
+"use client";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 import { FcCancel } from "react-icons/fc";
 
 const CancelPaymentPage = () => {
+  const router = useRouter();
+  const params = useSearchParams();
+
+  useEffect(() => {
+    const sessionID = params.get("sessionId");
+    if (!sessionID) {
+      router.push("/");
+    }
+  }, []);
   return (
     <div className="flex py-24 flex-col justify-start items-center">
       <FcCancel className="text-7xl text-red-500" />
